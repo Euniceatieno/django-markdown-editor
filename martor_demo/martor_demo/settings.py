9f71681550048e7bfc77b745a55fea692a94364d
Eunice Atieno <eunniceatieno@gmail.com>
2021-11-16 08:25:47 +0300

    added database

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'martor_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'editor',
+        'USER': 'eunice',
+        'PASSWORD': 'brenystar',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
